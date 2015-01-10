fd73f3cd2f91bf177677d5977b26a14db272829a
jorgecastilloprz <jorge.castillo.prz@gmail.com>
2015-01-10 12:43:36 +0100

    Eliminados comentarios redundántes y código no necesario insertados por el IDE.

---
+++ app/src/main/java/com/github/jorgecastilloprz/easymvp/PlaceHolderFragment.java
@@ -0,0 +1,38 @@
+package com.github.jorgecastilloprz.easymvp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PlaceHolderFragment extends Fragment {
+    
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public static PlaceHolderFragment newInstance(int sectionNumber) {
+        
+        PlaceHolderFragment fragment = new PlaceHolderFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((MainActivity) activity).onSectionAttached(getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+}
