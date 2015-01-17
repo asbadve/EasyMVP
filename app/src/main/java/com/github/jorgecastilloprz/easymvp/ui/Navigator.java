4eb314f83fcb212034b3b12147c8c9693cbad458
jorgecastilloprz <jorge.castillo.prz@gmail.com>
2015-01-17 12:41:17 +0100

    Navigator basic impl finished.

---
+++ app/src/main/java/com/github/jorgecastilloprz/easymvp/ui/Navigator.java
@@ -0,0 +1,29 @@
+package com.github.jorgecastilloprz.easymvp.ui;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.github.jorgecastilloprz.easymvp.di.qualifiers.ActivityContext;
+import com.github.jorgecastilloprz.easymvp.mvp.views.DetailsActivity;
+
+import javax.inject.Inject;
+
+/**
+ * Entity used to implement navigation between activities and fragments
+ * * * 
+ * Created by jorge on 17/01/15.
+ */
+public class Navigator {
+    
+    private final Context activityContext;
+    
+    @Inject
+    public Navigator(@ActivityContext Context activityContext) {
+        this.activityContext = activityContext;
+    }
+    
+    public void openDetailsActivity() {
+        Intent detailsActivityIntent = new Intent(activityContext, DetailsActivity.class);
+        activityContext.startActivity(detailsActivityIntent);
+    }
+}
